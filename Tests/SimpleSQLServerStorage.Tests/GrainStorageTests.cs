d4f741ebaa20cdf4d906cff6de1deb054093d5a4
Alex McCool <alex.mccool@yahoo.com>
2016-06-22 18:34:28 -0500

    move to TestingCluster from TestingSiloHost (#4)

--- Tests/SimpleSQLServerStorage.Tests/GrainStorageTests.cs
+++ Tests/SimpleSQLServerStorage.Tests/GrainStorageTests.cs
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Orleans;
 using Orleans.Streams;
+using Orleans.Runtime;
+using Orleans.Runtime.Configuration;
 
 namespace SimpleSQLServerStorage.Tests
 {
@@ -21,9 +23,22 @@ namespace SimpleSQLServerStorage.Tests
     [TestClass]
     public class GrainStorageTests
     {
-        public static TestingSiloHost testingHost;
 
-        private readonly TimeSpan timeout = Debugger.IsAttached ? TimeSpan.FromMinutes(5) : TimeSpan.FromSeconds(10);
+        const string STREAMPROVIDERNAME = @"streamprovidername";
+
+
+
+
+        #region Orleans Stuff
+        private static TestCluster testingCluster;
+
+        public Logger Logger
+        {
+            get { return GrainClient.Logger; }
+        }
+
+
+        private readonly TimeSpan _timeout = Debugger.IsAttached ? TimeSpan.FromMinutes(5) : TimeSpan.FromSeconds(10);
 
         private TestContext testContextInstance;
 
@@ -33,28 +48,16 @@ namespace SimpleSQLServerStorage.Tests
             set { testContextInstance = value; }
         }
 
+        public GrainStorageTests()
+        {
+        }
+
+
         [ClassInitialize]
         public static void SetUp(TestContext context)
         {
-            testingHost = new TestingSiloHost(new TestingSiloOptions
-            {
-                SiloConfigFile = new FileInfo("OrleansConfigurationForTesting.xml"),
-                StartFreshOrleans = true,
-
-                AdjustConfig = config =>
-                {
-                    config.Globals.RegisterStorageProvider<Orleans.StorageProviders.SimpleSQLServerStorage.SimpleSQLServerStorage>(providerName: "basic", properties:
-                        new Dictionary<string, string>                        {
-                            { "ConnectionString" , string.Format(@"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename={0};Trusted_Connection=Yes", Path.Combine(context.DeploymentDirectory, "basic.mdf"))},
-                            { "TableName", "basic"},
-                            { "UseJsonFormat", "both" }
-                        });
-                }
-            },
-            new TestingClientOptions()
-            {
-                ClientConfigFile = new FileInfo("ClientConfigurationForTesting.xml")
-            });
+            testingCluster = CreateTestCluster(context);
+            testingCluster.Deploy();
         }
 
         [ClassCleanup]
@@ -63,15 +66,43 @@ namespace SimpleSQLServerStorage.Tests
             // Optional. 
             // By default, the next test class which uses TestignSiloHost will
             // cause a fresh Orleans silo environment to be created.
-            testingHost.StopAllSilos();
+            testingCluster.StopAllSilos();
         }
+
+
+        public static TestCluster CreateTestCluster(TestContext context)
+        {
+            var options = new TestClusterOptions(10);
+
+            //options.ClusterConfiguration.AddMemoryStorageProvider("Default");
+            //options.ClusterConfiguration.AddSimpleMessageStreamProvider(STREAMPROVIDERNAME);
+
+            options.ClusterConfiguration.Globals.ClientDropTimeout = TimeSpan.FromSeconds(5);
+            options.ClusterConfiguration.Defaults.DefaultTraceLevel = Orleans.Runtime.Severity.Verbose;
+            options.ClusterConfiguration.Defaults.TraceLevelOverrides.Add(new Tuple<string, Severity>("StreamConsumerExtension", Severity.Verbose3));
+
+            options.ClusterConfiguration.Globals.RegisterStorageProvider<Orleans.StorageProviders.SimpleSQLServerStorage.SimpleSQLServerStorage>(
+                providerName: "basic", 
+                properties:
+                        new Dictionary<string, string>                        {
+                            { "ConnectionString" , string.Format(@"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename={0};Trusted_Connection=Yes", Path.Combine(context.DeploymentDirectory, "basic.mdf"))},
+                            { "TableName", "basic"},
+                            { "UseJsonFormat", "both" }
+                        });
+
+            //options.ClientConfiguration.AddSimpleMessageStreamProvider(STREAMPROVIDERNAME);
+            options.ClientConfiguration.DefaultTraceLevel = Orleans.Runtime.Severity.Verbose;
+
+            return new TestCluster(options);
+        }
+        #endregion
 
 
 
         [TestMethod]
         public async Task TestMethodGetAGrainTest()
         {
-            var g = testingHost.GrainFactory.GetGrain<IMyGrain>(0);
+            var g = testingCluster.GrainFactory.GetGrain<IMyGrain>(0);
 
             await g.SaveSomething(1, "ff", Guid.NewGuid(), DateTime.Now, new int[] { 1, 2, 3, 4, 5 });
         }
@@ -88,7 +119,7 @@ namespace SimpleSQLServerStorage.Tests
 
 
             // insert your grain test code here
-            var grain = testingHost.GrainFactory.GetGrain<IMyGrain>(rndId1);
+            var grain = testingCluster.GrainFactory.GetGrain<IMyGrain>(rndId1);
 
             var thing4 = new DateTime();
             var thing3 = Guid.NewGuid();
@@ -118,7 +149,7 @@ namespace SimpleSQLServerStorage.Tests
 
 
             // insert your grain test code here
-            var grain = testingHost.GrainFactory.GetGrain<IStateTestGrain>(rndId1);
+            var grain = testingCluster.GrainFactory.GetGrain<IStateTestGrain>(rndId1);
 
             var thing4 = new DateTime();
             var thing3 = Guid.NewGuid();
