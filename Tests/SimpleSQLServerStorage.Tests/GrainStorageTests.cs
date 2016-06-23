2d0d636ee9487f002572a1ac1f6d5404172c4d68
Alex McCool <alex.mccool@yahoo.com>
2016-06-22 17:34:27 -0700

    switch to xunit

--- Tests/SimpleSQLServerStorage.Tests/GrainStorageTests.cs
+++ Tests/SimpleSQLServerStorage.Tests/GrainStorageTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Orleans.TestingHost;
 using System.Diagnostics;
 using System.IO;
@@ -11,20 +10,20 @@ using Orleans;
 using Orleans.Streams;
 using Orleans.Runtime;
 using Orleans.Runtime.Configuration;
+using Xunit.Abstractions;
+using Xunit;
 
 namespace SimpleSQLServerStorage.Tests
 {
-    [DeploymentItem("ClientConfigurationForTesting.xml")]
-    [DeploymentItem("OrleansConfigurationForTesting.xml")]
-    [DeploymentItem("OrleansProviders.dll")]
-    [DeploymentItem("Orleans.StorageProviders.SimpleSQLServerStorage.dll")]
-    [DeploymentItem("SimpleGrains.dll")]
-    // EF is creating this file for us     [DeploymentItem("basic.mdf")]
-    [TestClass]
-    public class GrainStorageTests
+    //[DeploymentItem("ClientConfigurationForTesting.xml")]
+    //[DeploymentItem("OrleansConfigurationForTesting.xml")]
+    //[DeploymentItem("OrleansProviders.dll")]
+    //[DeploymentItem("Orleans.StorageProviders.SimpleSQLServerStorage.dll")]
+    //[DeploymentItem("SimpleGrains.dll")]
+    //// EF is creating this file for us     [DeploymentItem("basic.mdf")]
+    //[TestClass]
+    public class GrainStorageTests : IDisposable
     {
-        const string STREAMPROVIDERNAME = @"streamprovidername";
-
         #region Orleans Stuff
         private static TestCluster testingCluster;
 
@@ -36,39 +35,80 @@ namespace SimpleSQLServerStorage.Tests
 
         private readonly TimeSpan _timeout = Debugger.IsAttached ? TimeSpan.FromMinutes(5) : TimeSpan.FromSeconds(10);
 
-        private TestContext testContextInstance;
+        private readonly ITestOutputHelper output;
 
-        public TestContext TestContext
+        public GrainStorageTests(ITestOutputHelper output)
         {
-            get { return testContextInstance; }
-            set { testContextInstance = value; }
-        }
+            this.output = output;
 
-        public GrainStorageTests()
-        {
-        }
-
-
-        [ClassInitialize]
-        public static void SetUp(TestContext context)
-        {
-            testingCluster = CreateTestCluster(context);
+            testingCluster = CreateTestCluster();
             testingCluster.Deploy();
         }
 
-        [ClassCleanup]
-        public static void ClassCleanup()
+
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
         {
-            // Optional. 
-            // By default, the next test class which uses TestignSiloHost will
-            // cause a fresh Orleans silo environment to be created.
-            testingCluster.StopAllSilos();
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    // Optional. 
+                    // By default, the next test class which uses TestignSiloHost will
+                    // cause a fresh Orleans silo environment to be created.
+                    testingCluster.StopAllSilos();
+
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
         }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~FacilityGrainTest() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+
+
+        //[ClassInitialize]
+        //public static void SetUp(TestContext context)
+        //{
+        //    testingCluster = CreateTestCluster(context);
+        //    testingCluster.Deploy();
+        //}
+
+        //[ClassCleanup]
+        //public static void ClassCleanup()
+        //{
+        //    // Optional. 
+        //    // By default, the next test class which uses TestignSiloHost will
+        //    // cause a fresh Orleans silo environment to be created.
+        //    testingCluster.StopAllSilos();
+        //}
 
         //[TestInitialize]
 
 
-        public static TestCluster CreateTestCluster(TestContext context)
+        public static TestCluster CreateTestCluster()
         {
             var options = new TestClusterOptions(3);
 
@@ -81,7 +121,7 @@ namespace SimpleSQLServerStorage.Tests
 
             options.ClusterConfiguration.AddMemoryStorageProvider("memtester");
             options.ClusterConfiguration.AddSimpleSQLStorageProvider("basic",
-                string.Format(@"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename={0};Trusted_Connection=Yes", Path.Combine(context.DeploymentDirectory, "basic.mdf")), "true");
+                string.Format(@"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename={0};Trusted_Connection=Yes", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "basic.mdf")), "true");
 
             //options.ClientConfiguration.AddSimpleMessageStreamProvider(STREAMPROVIDERNAME);
             options.ClientConfiguration.DefaultTraceLevel = Orleans.Runtime.Severity.Verbose3;
@@ -92,7 +132,7 @@ namespace SimpleSQLServerStorage.Tests
 
 
 
-        [TestMethod]
+        [Fact]
         public async Task TestMethodGetAGrainTest()
         {
             var g = testingCluster.GrainFactory.GetGrain<IMyGrain>(0);
@@ -102,7 +142,7 @@ namespace SimpleSQLServerStorage.Tests
 
 
 
-        [TestMethod]
+        [Fact]
         public async Task TestGrains()
         {
             var rnd = new Random();
@@ -122,17 +162,17 @@ namespace SimpleSQLServerStorage.Tests
 
             await grain.SaveSomething(thing1, thing2, thing3, thing4, things);
 
-            Assert.AreEqual(thing1, await grain.GetThing1());
-            Assert.AreEqual(thing2, await grain.GetThing2());
-            Assert.AreEqual(thing3, await grain.GetThing3());
-            Assert.AreEqual(thing4, await grain.GetThing4());
+            Assert.Equal(thing1, await grain.GetThing1());
+            Assert.Equal(thing2, await grain.GetThing2());
+            Assert.Equal(thing3, await grain.GetThing3());
+            Assert.Equal(thing4, await grain.GetThing4());
             var res = await grain.GetThings1();
-            CollectionAssert.AreEqual(things, res.ToList());
+            Assert.Equal(things, res.ToList());
         }
 
 
 
-        [TestMethod]
+        [Fact]
         public async Task StateTestGrainTest()
         {
             var rnd = new Random();
@@ -152,15 +192,15 @@ namespace SimpleSQLServerStorage.Tests
 
             await grain.SaveSomething(thing1, thing2, thing3, thing4, things);
 
-            Assert.AreEqual(thing1, await grain.GetThing1());
-            Assert.AreEqual(thing2, await grain.GetThing2());
-            Assert.AreEqual(thing3, await grain.GetThing3());
-            Assert.AreEqual(thing4, await grain.GetThing4());
+            Assert.Equal(thing1, await grain.GetThing1());
+            Assert.Equal(thing2, await grain.GetThing2());
+            Assert.Equal(thing3, await grain.GetThing3());
+            Assert.Equal(thing4, await grain.GetThing4());
             var res = await grain.GetThings1();
-            CollectionAssert.AreEqual(things, res.ToList());
+            Assert.Equal(things, res.ToList());
         }
 
-        [TestMethod]
+        [Fact]
         public async Task ClearStateTest()
         {
             var rnd = new Random();
