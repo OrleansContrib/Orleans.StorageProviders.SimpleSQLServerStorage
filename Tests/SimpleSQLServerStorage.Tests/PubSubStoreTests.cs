647fb93094abbc59e7102c80328c397391fc1969
jeoffman <jeoffman@users.noreply.github.com>
2016-07-28 18:40:35 -0500

    Moved AddSimpleSQLStorageProvider into the main SimpleSQLServerStorage project (#7)

--- Tests/SimpleSQLServerStorage.Tests/PubSubStoreTests.cs
+++ Tests/SimpleSQLServerStorage.Tests/PubSubStoreTests.cs
@@ -13,6 +13,7 @@ using Orleans.Runtime;
 using Orleans.Runtime.Configuration;
 using Xunit;
 using Xunit.Abstractions;
+using Orleans.StorageProviders.SimpleSQLServerStorage;
 
 namespace SimpleSQLServerStorage.Tests
 {
